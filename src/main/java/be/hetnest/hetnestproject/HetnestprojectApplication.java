65c0beeb50508a6715e2330b26972f89662ad5a2
tanguy <tanguy.ortegat@student.odisee.be>
2019-05-08 19:02:08 +0200

    Initial commit

---
+++ src/main/java/be/hetnest/hetnestproject/HetnestprojectApplication.java
@@ -0,0 +1,13 @@
+package be.hetnest.hetnestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HetnestprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HetnestprojectApplication.class, args);
+	}
+
+}
