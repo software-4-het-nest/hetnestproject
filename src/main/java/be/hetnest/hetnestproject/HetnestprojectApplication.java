81e79a6931b840e50c35ccabda364de07d39308b
GrainP <32297148+GrainP@users.noreply.github.com>
2019-05-20 12:59:03 +0200

    role based elements ++

--- src/main/java/be/hetnest/hetnestproject/HetnestprojectApplication.java
+++ src/main/java/be/hetnest/hetnestproject/HetnestprojectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"be.hetnest.hetnestproject.controller", "be.hetnest.hetnestproject.service","be.hetnest.hetnestproject"})
+/*@ComponentScan(basePackages= {"be.hetnest.hetnestproject.controller", "be.hetnest.hetnestproject.service","be.hetnest.hetnestproject"})*/
 public class HetnestprojectApplication {
 
 	public static void main(String[] args) {
