4364f0341c8f9220cb19e5dbefbd726f5cf063be
redouaneamanchar <redouane.amanchar@student.odisee.be>
2019-05-21 21:26:21 +0200

    consume werkend buiten ons project
    Dit project was enkel bedoeld om de simulator te testen. Dit is al geïmplementeerd in onze eigen project via de nieuweAanvraag pagina.

--- hetnestproject_consume/src/main/java/hetnestproject_consume/Aanvraag.java
+++ hetnestproject_consume/src/main/java/hetnestproject_consume/Aanvraag.java
@@ -1,54 +1,88 @@
 package hetnestproject_consume;
 
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
-import javax.xml.bind.annotation.*;
+
 
 @XmlRootElement(name = "aanvraag")
 public class Aanvraag implements Serializable {
 
 
-    private static final long serialVersionUID = 1L;
+    private long id;
 
-    private int id;
-    private String vruchtensap;
-    private double quantity;
+    private int hoeveelheid;
 
+    private double prijs;
 
-    public Aanvraag() {
+    private String type;
+
+    private String naam;
+
+    public Aanvraag()
+    {
 
     }
-
-    public Aanvraag(String vruchtensap, double quantity) {
-        this.vruchtensap = vruchtensap;
-        this.quantity = quantity;
+    public Aanvraag(int _hoeveelheid, double _prijs, String _type, String _naam)
+    {
+        this.hoeveelheid = _hoeveelheid;
+        this.prijs = _prijs;
+        this.type = _type;
+        this.naam = _naam;
     }
 
     @XmlElement(name = "id")
-    public int getId() {
-        return id;
+    public long getId() {
+        return this.id;
     }
 
-    @XmlElement(name = "vruchtensap")
-    public String getVruchtensap() {
-        return vruchtensap;
+    public void setId(long _id)
+    {
+        this.id =_id;
     }
 
-    @XmlElement(name = "quantity")
-    public double getQuantity() {
-        return quantity;
+    @XmlElement(name = "hoeveelheid")
+    public int getHoeveelheid()
+    {
+        return this.hoeveelheid;
     }
 
-    public  void  setId(int id) {this.id = id; }
-
-    public void setVruchtensap(String vruchtensap) {
-        this.vruchtensap = vruchtensap;
+    @XmlElement(name = "prijs")
+    public double getPrijs()
+    {
+        return this.prijs;
     }
 
-    public void setQuantity(double quantity) {
-        this.quantity = quantity;
+    @XmlElement(name = "type")
+    public String getType()
+    {
+        return this.type;
+    }
+
+    @XmlElement(name = "naam")
+    public String getNaam()
+    {
+        return this.naam;
+    }
+
+    public void setHoeveelheid(int _hoeveelheid)
+    {
+        this.hoeveelheid = _hoeveelheid;
+    }
+
+    public void setPrijs(double _prijs)
+    {
+        this.prijs = _prijs;
+    }
+
+    public void setType(String _type)
+    {
+        this.type = _type;
     }
 
 
+    public void setNaam(String naam){
+        this.naam = naam;
+    }
 }
